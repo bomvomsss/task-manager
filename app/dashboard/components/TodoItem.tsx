21aaf5b431fe0aebae420798a8cb697f1e015411
Kyla Kim <kimky9805@gmail.com>
2025-07-04 13:57:00 +0900

    대시보드, 캘린더 날짜 지난 투두 경고표시

--- app/dashboard/components/TodoItem.tsx
+++ app/dashboard/components/TodoItem.tsx
@@ -1,24 +1,37 @@
 import { TodoItemType } from "../../hooks/useCtrlItems";
+import { BsExclamationCircle } from "react-icons/bs";
+import { useRef } from "react";
 
+function getDateOnly(dateStr: string) {
+  return dateStr.split("T")[0];
+}
 interface TodoItemProps extends TodoItemType {
   onDoubleClick: () => void;
   draggable?: boolean;
   onDragStart?: (e: React.DragEvent) => void;
 }
-
 export default function TodoItem({
   title,
   onDoubleClick,
   draggable,
   onDragStart,
+  end_date,
 }: TodoItemProps) {
+  const itemRef = useRef<HTMLDivElement>(null);
+  const todayStr = new Date().toISOString().split("T")[0];
+  const isOverdue =
+    end_date && getDateOnly(end_date) < todayStr && status !== "done";
   return (
     <div
+      ref={itemRef}
       className='todo-item'
       onDoubleClick={onDoubleClick}
       draggable={draggable}
       onDragStart={onDragStart}
     >
+      {isOverdue && (
+        <BsExclamationCircle color='red' style={{ marginRight: 4 }} />
+      )}
       {title}
     </div>
   );
