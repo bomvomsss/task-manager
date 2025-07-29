811647a6a9b6495446f65f72a8ec1220b7218688
Kyla Kim <kimky9805@gmail.com>
2025-07-29 11:49:59 +0900

    리다이렉트 오류 수정

--- app/old-path/page.tsx
+++
@@ -1,5 +0,0 @@
-import { permanentRedirect } from "next/navigation";
-export default function OldPathRedirect() {
-    permanentRedirect("/main");
-    return null;
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import { permanentRedirect } from "next/navigation";
 
-export default function CatchAll({ params }: { params?: { slug?: string[] } }) {
-  const path = params?.slug?.join("/") ?? "";
-  if (path === "old-path") permanentRedirect("/main");
-  return <h1>404</h1>;
+export default function Home() {
+  permanentRedirect("/main");
 }
