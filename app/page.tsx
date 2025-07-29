f6848404bb9cf9be01ce2eb5f3da20461e5b99d8
Kyla Kim <kimky9805@gmail.com>
2025-07-29 11:44:17 +0900

    type 오류 해결

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { permanentRedirect } from "next/navigation";
 
-export default function CatchAll({ params }: { params: { slug: string[] } }) {
-  const path = params.slug.join("/");
+export default function CatchAll({ params }: { params?: { slug?: string[] } }) {
+  const path = params?.slug?.join("/") ?? "";
   if (path === "old-path") permanentRedirect("/new-path");
   return <h1>404</h1>;
 }
